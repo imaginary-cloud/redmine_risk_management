cb6beb4b4e19c21fd851f9462b6f72b39f647853
Timóteo Bica <tim@timoteobica.info>
2015-02-04 17:11:57 +0000

    Adds Redmine Risk Management plugin skeleton.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_risk_management do
+  name 'Redmine Risk Management'
+  author 'Imaginary Cloud (http://imaginarycloud.com)'
+  description 'Risk management plugin for Redmine'
+  version '0.1'
+  url 'https://github.com/imaginary-cloud/redmine_risk_management'
+  author_url 'mailto:info@imaginarycloud.com'
+end
