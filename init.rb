0e3925da86532dcc8fad87063feca7d228e3764f
Timóteo Bica <tim@timoteobica.info>
2015-02-06 14:40:43 +0000

    Adds more permissions and settings in init.rb.

--- init.rb
+++ init.rb
@@ -6,10 +6,13 @@ Redmine::Plugin.register :redmine_risk_management do
   url 'https://github.com/imaginary-cloud/redmine_risk_management'
   author_url 'mailto:info@imaginarycloud.com'
 
-  project_module :risk_management do
-    permission :view_risks, { risks: :index }, { public: true }
+  project_module :risks do
+    permission :view_risks, { risks: [:index, :show] }, { public: true }
+    permission :add_risks, { risks: [:new, :create] }, { public: true }
+    permission :edit_risks, { risks: [:edit, :update] }, { public: true }
+    permission :delete_risks, { risks: :destroy }, { public: true }
   end
 
   menu :project_menu, :risks, { controller: :risks, action: :index },
-       caption: 'Risks', before: :settings, param: :project_id
+       caption: :label_risk_plural, before: :settings, param: :project_id
 end
