8abc38c9118aea891bb85e3167501bdef028e5fd
Timóteo Bica <tim@timoteobica.info>
2015-02-18 14:54:46 +0000

    Adds RiskStatus uniqueness valition test.

--- test/unit/risk_status_test.rb
+++ test/unit/risk_status_test.rb
@@ -1,14 +1,26 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class RiskStatusTest < ActiveSupport::TestCase
+  fixtures :risk_statuses
+
+  setup do
+    @risk_status = risk_statuses(:risk_statuses_001)
+  end
 
   test 'should be created' do
     risk_status = RiskStatus.new(name: 'Risk name', color: '#FFF', status_type: 'open')
     assert risk_status.save
   end
 
-  test 'should have the necessary presence validators' do
+  test 'should validate name presence' do
     risk_status = RiskStatus.new
+    assert risk_status.invalid?
+    assert_equal [:name], risk_status.errors.keys.sort
+  end
+
+  test 'should validate name uniqueness' do
+    @risk_status.update_attribute(:name, 'Risk 1')
+    risk_status = RiskStatus.new(name: 'Risk 1')
     assert risk_status.invalid?
     assert_equal [:name], risk_status.errors.keys.sort
   end
