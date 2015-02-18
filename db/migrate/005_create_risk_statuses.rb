3b8cc06abed50214a162def939f865b1180ec50b
Timóteo Bica <tim@timoteobica.info>
2015-02-18 14:40:16 +0000

    Fixes bug in CreateRiskStatuses migration and remove is_closed column.

--- db/migrate/005_create_risk_statuses.rb
+++ db/migrate/005_create_risk_statuses.rb
@@ -2,13 +2,11 @@ class CreateRiskStatuses < ActiveRecord::Migration
   def change
     create_table :risk_statuses do |t|
       t.string  :name
-      t.integer :position,
+      t.integer :position
       t.boolean :is_default,  default: false
-      t.boolean :is_closed,   default: false
       t.integer :color,       default: 11184810
       t.string  :status_type, default: 'open'
     end
-    add_index :risk_statuses, :is_closed
 
     add_column :risks, :risk_status_id, :integer
     add_index :risks, :risk_status_id
--- db/migrate/007_remove_is_closed_from_risk_statuses.rb
+++
@@ -1,8 +0,0 @@
-class RemoveIsClosedFromRiskStatuses < ActiveRecord::Migration
-
-  def change
-    remove_column :risk_statuses, :is_closed
-    remove_index :risk_statuses, :is_closed
-  end
-
-end
